ecc3969620cc1f5230366837a7927d07f0967def
mrspits4ever <spits.lucas@gmail.com>
2016-09-06 14:00:46 +0200

    custom libary upload
    added the custom libary

---
+++ CustomLibary/CustomLibrary/EnemyAI/EnemyAI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace CustomLibrary.EnemyAI {
+    public class EnemyAI : MonoBehaviour{
+        
+        //Enemy AI stuff komt hier
+
+        public static void Enemy_Move(ref GameObject enemy, float speed) {
+
+        }
+
+    }
+}
---
+++ CustomLibary/CustomLibrary/Tools.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Windows.Forms;
+
+namespace CustomLibrary {
+
+    public class Tools {
+
+        private static readonly string currentVersion = "0.15";
+
+        public static void ShowMessage(string message) {
+            MessageBox.Show(message, "Custom Library v"+currentVersion, MessageBoxButtons.OK);
+        }
+
+        public static void ShowMessage(string message, string caption) {
+            MessageBox.Show(message, caption, MessageBoxButtons.OK);
+        }
+    }
+}
