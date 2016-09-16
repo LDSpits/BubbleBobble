a1d6228533a94553217befdd822efa0ac7c8a85a
mrspits4ever <spits.lucas@gmail.com>
2016-09-16 13:25:58 +0200

    verbetering aan de slang

--- Assets/Scripts/PowerUps/WaterSnake.cs
+++ Assets/Scripts/PowerUps/WaterSnake.cs
@@ -6,12 +6,21 @@ public class WaterSnake : MonoBehaviour {
 
     private Vector3 direction;
     private float seconds = 0;
+    private bool hasDestination = false;
+    private SpriteRenderer sr;
+    private Sprite bodySprite;
+
+    public Sprite waterHeadStandard;
+    public Sprite waterFallingBody;
+    public Sprite waterTurning;
 
     public List<GameObject> waterBlocks = new List<GameObject>();
     public GameObject waterBlockFollowerPrefab;
-    private bool hasDestination = false;
 
     void Start () {
+
+        sr = GetComponent<SpriteRenderer>();
+
         waterBlocks.Add(gameObject); //Voeg eerst de 'kop' toe
         AddWaterBlock();
         AddWaterBlock();
@@ -22,25 +31,53 @@ public class WaterSnake : MonoBehaviour {
 
         seconds += Time.deltaTime;
 
+        if(seconds < 0.025f)
+        {
+            return;
+        }
+        seconds = 0;
+        Follow();
+
         if (!GoodCollisions.CheckSide(this, Vector2.down,0.5f,"Solid"))
         {
+            if(hasDestination == true)
+                waterBlocks[1].GetComponent<SpriteRenderer>().sprite = waterTurning;
+
             transform.position += Vector3.down * 0.5f;
+            transform.eulerAngles = new Vector3(0,0,180);
+            bodySprite = waterFallingBody;
             hasDestination = false;
         }
         else //We raken de grond
         {
+            //als die geen richting heeft
             if (!hasDestination)
             {
+                //kies een richting
                 direction = Choose();
+                waterBlocks[1].GetComponent<SpriteRenderer>().sprite = waterTurning;
+
+                //bepaal rotatie voor het hoofd van de slang aan de hand van de richting
+                if (direction == Vector3.left)
+                    transform.eulerAngles = new Vector3(0,0,90);
+                else if (direction == Vector3.right)
+                    transform.eulerAngles = new Vector3(0, 0, 270);
+
+                
+
                 hasDestination = true;
+                transform.position += direction * 0.5f;
             }
             else
             {
+                sr.sprite = waterHeadStandard;
+                bodySprite = waterFallingBody;
+                
                 transform.position += direction * 0.5f;
             }
         }
 
-        Follow();
+        
 
     }
 
@@ -64,11 +101,16 @@ public class WaterSnake : MonoBehaviour {
         return Vector2.down;
     }
 
+    private Vector3 CalcRotation()
+    {
+        return new Vector3();
+    }
+
     private void Follow(){  
         //Bewaar de positie voordat we begen voor elke iteratie
         Vector3 lastPos = transform.position;
         Quaternion lastRot = transform.rotation;
-        Sprite lastSprite = GetComponent<SpriteRenderer>().sprite;
+        Sprite lastSprite = bodySprite;
 
         for (int i = 1; i < waterBlocks.Count; i++){ //Begin vanaf index 1, volg de kop
             //Sla de huidige positie op als laatste positie
@@ -91,6 +133,7 @@ public class WaterSnake : MonoBehaviour {
     {
         GameObject block = Instantiate(waterBlockFollowerPrefab) as GameObject;
         block.transform.parent = transform.parent;
+        block.transform.localScale = new Vector3(1.01f, 1.01f);
         waterBlocks.Add(block);
     }
 
