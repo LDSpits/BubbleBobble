274edf4646e5e605d68ef7dd48f7cd895d9a4fa4
mrspits4ever <spits.lucas@gmail.com>
2016-10-11 15:49:11 +0200

    all changes uploaded

--- CustomLibary/CustomLibrary/Collisions/Collisions.cs
+++ CustomLibary/CustomLibrary/Collisions/Collisions.cs
@@ -16,11 +16,9 @@ namespace CustomLibrary.Collisions
             GameObject gameObject = thisComponent.gameObject;
 
             Vector2 sideOffset = CalcSideOffset(direction);
-            float _distance = CalcDistance(sideOffset, gameObject.GetComponent<BoxCollider2D>());
+            float _distance = CalcDistance(sideOffset, gameObject.GetComponent<BoxCollider2D>()); 
 
-            RaycastHit2D hit = PerformRaycasts(gameObject.transform.position, direction, sideOffset, _distance, "");
-
-            return hit;
+            return PerformRaycasts(gameObject.transform.position, direction, sideOffset, _distance, new string[1]);
         }
 
         /// <summary>
@@ -28,18 +26,16 @@ namespace CustomLibrary.Collisions
         /// </summary>
         /// <param name="thisComponent">dit object</param>
         /// <param name="direction">De richting waarin we controleren of er iets is</param>
-        /// <param name="withTag">Welk tag het obstakel moet hebben</param>
+        /// <param name="withTag">Welk tag het obstakel moet hebben, dit mogen er meerdere zijn</param>
         /// <returns>Of er iets in de weg staat (true) of niet (false)</returns>
-        public static bool CheckSide(Component thisComponent, Vector2 direction, string withTag)
+        public static bool CheckSide(Component thisComponent, Vector2 direction, string[] withTag)
         {
             GameObject gameObject = thisComponent.gameObject;
 
             Vector2 sideOffset = CalcSideOffset(direction);
             float _distance = CalcDistance(sideOffset, gameObject.GetComponent<BoxCollider2D>());
 
-            RaycastHit2D hit = PerformRaycasts(gameObject.transform.position, direction, sideOffset, _distance, withTag);
-
-            return (hit && hit.collider.CompareTag(withTag));
+            return PerformRaycasts(gameObject.transform.position, direction, sideOffset, _distance, withTag);
         }
 
         /// <summary>
@@ -49,16 +45,14 @@ namespace CustomLibrary.Collisions
         /// <param name="direction">De richting waarin we controleren of er iets is</param>
         /// <param name="withTag">Welk tag het obstakel moet hebben</param>
         /// <returns>Of er iets in de weg staat (true) of niet (false)</returns>
-        public static bool CheckSide(Vector3 origin,Component thisComponent, Vector2 direction, string withTag)
+        public static bool CheckSide(Vector3 origin,Component thisComponent, Vector2 direction, string[] withTag)
         {
             GameObject gameObject = thisComponent.gameObject;
 
             Vector2 sideOffset = CalcSideOffset(direction);
             float _distance = CalcDistance(sideOffset, gameObject.GetComponent<BoxCollider2D>());
 
-            RaycastHit2D hit = PerformRaycasts(origin, direction, sideOffset, _distance, withTag);
-
-            return (hit && hit.collider.CompareTag(withTag));
+            return PerformRaycasts(origin, direction, sideOffset, _distance, withTag);
         }
 
         /// <summary>
@@ -68,19 +62,17 @@ namespace CustomLibrary.Collisions
         /// <param name="direction">De richting waarin we controleren of er iets is</param>
         /// <param name="withTag">Welk tag het obstakel moet hebben</param>
         /// <returns>Of er iets in de weg staat (true) of niet (false)</returns>
-        public static bool CheckSide(Component thisComponent, Vector2 direction,float distance, string withTag)
+        public static bool CheckSide(Component thisComponent, Vector2 direction,float distance, string[] withTag)
         {
             GameObject gameObject = thisComponent.gameObject;
 
             Vector2 sideOffset = CalcSideOffset(direction);
             float _distance = distance;
 
-            RaycastHit2D hit = PerformRaycasts(gameObject.transform.position, direction, sideOffset, _distance, withTag);
-
-            return (hit && hit.collider.CompareTag(withTag));
+            return PerformRaycasts(gameObject.transform.position, direction, sideOffset, _distance, withTag);
         }
 
-        private static RaycastHit2D PerformRaycasts(Vector3 origin, Vector2 direction, Vector2 sideoffset, float distance, string tag)
+        private static bool PerformRaycasts(Vector3 origin, Vector2 direction, Vector2 sideoffset, float distance, string[] tags)
         {
             Color drawColor = new Color(0, 1, 0);
 
@@ -90,16 +82,31 @@ namespace CustomLibrary.Collisions
             (hit = Physics2D.Raycast((Vector2)origin + sideoffset * distance, direction, distance + 0.05f )) ||
             (hit = Physics2D.Raycast((Vector2)origin + -sideoffset * distance, direction, distance + 0.05f )));
 
+            bool hasTag = false;
+            // op het moment dat we iets raken
             if (hitSomething) {
-                if (tag == "" || hit.collider.CompareTag(tag))
-                    drawColor = new Color(1, 0, 0); //Rays rood tekenen
+                // checken we voor alle tags die zijn meegegeven 
+                foreach (string tag in tags) {
+
+                    // of de tag niet leeg is, zo ja breek de loop
+                    if (string.IsNullOrEmpty(tag)) {
+                        break;
+                    }
+                        
+                    //anders check je of de collider deze specifieke tag heeft
+                    if (hit.collider.CompareTag(tag)) {
+                        hasTag = true;
+                        break;
+                    }    
+                }
+                drawColor = new Color(1, 0, 0); //Rays rood tekenen
             }
 
             Debug.DrawRay(origin, direction * distance, drawColor);
             Debug.DrawRay((Vector2)origin + (sideoffset * distance), direction * (distance + 0.05f), drawColor);
             Debug.DrawRay((Vector2)origin + (-sideoffset * distance), direction * (distance + 0.05f), drawColor);
 
-            return hit;
+            return string.IsNullOrEmpty(tags[0]) || hasTag;
         }
 
         private static Vector2 CalcSideOffset(Vector2 direction)
@@ -111,10 +118,10 @@ namespace CustomLibrary.Collisions
         private static float CalcDistance(Vector2 sideOffset, BoxCollider2D collider)
         {
             if (sideOffset == Vector2.up)
-            {
                 return (collider.size.y / 2);
-            }
+
             return (collider.size.x / 2);
         }
+
     }
 }      
---
+++ CustomLibary/CustomLibrary/Timers/CustomTimer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using UnityEngine;
+
+namespace CustomLibrary.Timers
+{
+    public class GoodTimer : UnityEngine.Object
+    {
+        float elapsedSeconds = 0;
+        float _seconds;
+        Component _comp;
+        string _method;
+
+        public GoodTimer(Component thisComponent, float seconds, string method)
+        {
+            _comp = thisComponent;
+            _method = method;
+            _seconds = seconds;
+        }
+
+        void Update()
+        {
+            elapsedSeconds += Time.deltaTime;
+            if(elapsedSeconds > _seconds) {
+                _comp.gameObject.GetComponent<MonoBehaviour>().Invoke(_method, 0);
+            }
+        }
+
+        public void Start(float seconds)
+        {
+           
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+}
